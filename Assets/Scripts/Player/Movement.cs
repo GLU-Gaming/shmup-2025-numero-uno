e3ec4005975bbd55662cc560a9a5c81ebc2e18e0
jiminash-jimbaro <rapalaktjabare@gmail.com>
2025-03-19 09:47:22 +0100

    e
    e

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -2,5 +2,63 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private Rigidbody rb;
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            {
+                rb.AddForce(new Vector2(5, 0));
+            }
+
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(new Vector2(0, 5));
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            {
+                rb.AddForce(new Vector2(-5, 0));
+            }
+
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.AddForce(new Vector2(0, -5));
+        }
+
+
+
+        if (Input.GetKey(KeyCode.Alpha6))
+        {
+            {
+                rb.AddForce(new Vector2(5, 0));
+            }
+
+        }
+        if (Input.GetKey(KeyCode.Alpha8))
+        {
+            rb.AddForce(new Vector2(0, 5));
+        }
+
+        if (Input.GetKey(KeyCode.Alpha4))
+        {
+            {
+                rb.AddForce(new Vector2(-5, 0));
+            }
+
+        }
+        if (Input.GetKey(KeyCode.Alpha5))
+        {
+            rb.AddForce(new Vector2(0, -5));
+        }
+    }
 }
