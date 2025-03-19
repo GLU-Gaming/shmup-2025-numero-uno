652864fc2dc5a2a2647dedac3570348f08357c6d
jiminash-jimbaro <rapalaktjabare@gmail.com>
2025-03-19 12:38:26 +0100

    push
    push

--- Assets/Scripts/Background/Background movement.cs
+++ Assets/Scripts/Background/Background movement.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 public class Backgroundmovement : MonoBehaviour
 {
     private Rigidbody rb;
-
+    public float Speed = 120;
 
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
 
-        rb.AddForce(new Vector2(-120, 0));
+        rb.AddForce(new Vector2(Speed , 0));
     }
     void Update()
     {
