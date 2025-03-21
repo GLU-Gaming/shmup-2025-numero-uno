5ff836f7a05bc5b4ebc65b1f50e9102a46a98d70
jiminash-jimbaro <rapalaktjabare@gmail.com>
2025-03-21 16:46:34 +0100

    a
    a

---
+++ Assets/Scripts/Player/JimiScripts/EnemyMovement.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public GameObject PointA;
+    public GameObject PointB;
+    private Rigidbody rb;
+    [SerializeField] private Transform CurrentPoint;
+    public float speed;
+    public float UNspeed;
+
+
+    void Start()
+    {
+       rb = GetComponent<Rigidbody>();
+        CurrentPoint = PointB.transform;
+    }
+
+ 
+    void Update()
+    {
+   
+        Debug.Log(CurrentPoint);
+        Vector2 point = CurrentPoint.position - transform.position;
+        if (CurrentPoint == PointB.transform)
+        {
+            rb.AddForce(new Vector2(0, UNspeed));
+        }
+        else
+        {
+
+            if (CurrentPoint == PointA.transform)
+            {
+                rb.AddForce(new Vector2(0, speed));
+            }
+        }
+
+
+
+        if (Vector2.Distance(transform.position, CurrentPoint.position) < 0.5f && CurrentPoint == PointB.transform)
+        {
+            CurrentPoint = PointA.transform;    
+        }
+
+        if (Vector2.Distance(transform.position, CurrentPoint.position) < 0.5f && CurrentPoint == PointA.transform)
+        {
+            CurrentPoint = PointB.transform;
+        }
+    }
+}
