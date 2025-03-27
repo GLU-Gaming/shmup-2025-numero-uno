042de727f15528f00f5c01f4e0087cffa4e9d942
ThunderstarVT <155100050+ThunderstarVT@users.noreply.github.com>
2025-03-27 12:35:00 +0100

    edited BossGeneral

--- Assets/Scripts/Enemy/BossGeneral.cs
+++ Assets/Scripts/Enemy/BossGeneral.cs
@@ -5,19 +5,29 @@ public class BossGeneral : MonoBehaviour
     [SerializeField] private int maxHealth;
     private int health = 0;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        gameObject.SetActive(false);
-    }
+    public bool invincible = false;
 
     public void OnHit()
     {
-        health--;
-        if (health <= 0) Death();
+        if (!invincible)
+        {
+            health--;
+            if (health <= 0) Death();
+        }
     }
 
     public void Death()
     {
+        foreach (GameObject enemy in GameObject.FindGameObjectsWithTag("Enemy"))
+        {
+            enemy.GetComponent<EnemyGeneral>().Death();
+        }
+
+        foreach (GameObject enemy in GameObject.FindGameObjectsWithTag("EnemyProjectile"))
+        {
+            enemy.GetComponent<BatchChild>().Deactivate();
+        }
+
         gameObject.SetActive(false);
     }
 }
