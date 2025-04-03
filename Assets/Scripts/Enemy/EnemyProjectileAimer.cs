5855baa3b911125701d6bb0b5c9008da9a6143ca
ThunderstarVT <155100050+ThunderstarVT@users.noreply.github.com>
2025-04-03 10:05:03 +0200

    aiming projectile

---
+++ Assets/Scripts/Enemy/EnemyProjectileAimer.cs
@@ -0,0 +1,55 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyProjectileAimer : EnemyProjectileBase
+{
+    [SerializeField] float normalSpeed;
+    [SerializeField] float slowSpeed;
+
+    [SerializeField] float timeUntilTurn;
+
+    [SerializeField] Transform target;
+
+    private float timer;
+
+    private bool turned;
+
+    new void Start()
+    {
+        base.Start();
+
+        
+    }
+
+    void OnEnable() // put things that are normally in Start here
+    {
+        timer = timeUntilTurn;
+        turned = false;
+    }
+
+    public override void MoveBehaviour()
+    {
+        float speed = 0;
+
+        if (turned)
+        {
+            speed = normalSpeed;
+        } else
+        {
+            timer -= Time.deltaTime;
+
+            speed = Mathf.Lerp(slowSpeed, normalSpeed, timer / timeUntilTurn);
+
+            if (timer < 0)
+            {
+                Vector3 direction = target.position - transform.position;
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.Euler(0, 0, angle);
+
+                turned = true;
+            }
+        }
+
+        rb.linearVelocity = speed * transform.right;
+    }
+}
