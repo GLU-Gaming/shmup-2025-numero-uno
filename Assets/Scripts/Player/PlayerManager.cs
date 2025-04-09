83de12ce217d8c65c8b6481d464b31283acb3b39
ThunderstarVT <155100050+ThunderstarVT@users.noreply.github.com>
2025-04-09 08:52:57 +0200

    lazer indicator

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -48,6 +48,8 @@ public class PlayerManager : MonoBehaviour
     private float lazerActiveTimer;
     [SerializeField] float lazerActive;
 
+    [SerializeField] GameObject lazerIndicator;
+
     private void Start()
     {
         PlayerHealthTXT.text = "HP:" + playerHealth;
@@ -134,6 +136,8 @@ public class PlayerManager : MonoBehaviour
                 lazer = false;
             }
         }
+
+        lazerIndicator.SetActive(lazer);
 
         lazerObj.SetActive(lazerActiveTimer > 0);
         lazerActiveTimer -= Time.deltaTime;
